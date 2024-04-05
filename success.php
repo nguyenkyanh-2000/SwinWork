ae757322ab68014e0192787ce766c92263a016cf
lea <73567807+lebyleafy@users.noreply.github.com>
2024-04-05 18:29:09 +0700

    Create success.php for HR

---
+++ success.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <link rel="stylesheet" href="table.css?v=<?php echo time(); ?>">
+<title>SUCCESS</title>
+</head>
+<body>
+    <h1 class="heading_SC">Database Updated Successfully</h1>
+    <form action="manage.php">
+        <input type="submit" class="SCbutton button1" value="Back to database"/>
+    </form>
+<div class="divider-sm"></div>
+<?php include 'footer.inc'; ?>
+</body>
+</html>
