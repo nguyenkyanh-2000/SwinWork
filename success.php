49aad83855f351a06baf7d7776a25e8c669494b2
lea <73567807+lebyleafy@users.noreply.github.com>
2024-04-05 18:52:09 +0700

    Update success.php

--- success.php
+++ success.php
@@ -1,17 +1,99 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-   <meta charset="UTF-8" />
-   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-   <link rel="stylesheet" href="table.css?v=<?php echo time(); ?>">
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="table.css?v=1712317897">
 <title>SUCCESS</title>
 </head>
 <body>
     <h1 class="heading_SC">Database Updated Successfully</h1>
     <form action="manage.php">
-        <input type="submit" class="SCbutton button1" value="Back to database"/>
+        <input type="submit" class="SCbutton button1" value="Back to database">
     </form>
 <div class="divider-sm"></div>
-<?php include 'footer.inc'; ?>
-</body>
+<footer class="footer">
+  <div class="footer__addr">
+    <h1 class="logo">SwinWORK</h1>
+  </div>
+
+  <ul class="footer__nav">
+    <li class="nav__item">
+      <h2 class="nav__title">Media</h2>
+
+      <ul class="nav__ul">
+        <li>
+          <a href="#">Online</a>
+        </li>
+
+        <li>
+          <a href="#">Print</a>
+        </li>
+
+        <li>
+          <a href="#">Alternative Ads</a>
+        </li>
+      </ul>
+    </li>
+
+    <li class="nav__item nav__item--extra">
+      <h2 class="nav__title">Technology</h2>
+
+      <ul class="nav__ul nav__ul--extra">
+        <li>
+          <a href="#">Hardware Design</a>
+        </li>
+
+        <li>
+          <a href="#">Software Design</a>
+        </li>
+
+        <li>
+          <a href="#">Digital Signage</a>
+        </li>
+
+        <li>
+          <a href="#">Automation</a>
+        </li>
+
+        <li>
+          <a href="#">Artificial Intelligence</a>
+        </li>
+
+        <li>
+          <a href="#">IoT</a>
+        </li>
+      </ul>
+    </li>
+
+    <li class="nav__item">
+      <h2 class="nav__title">Legal</h2>
+
+      <ul class="nav__ul">
+        <li>
+          <a href="#">Privacy Policy</a>
+        </li>
+
+        <li>
+          <a href="#">Terms of Use</a>
+        </li>
+
+        <li>
+          <a href="enhancements.php">Enhancements</a>
+        </li>
+      </ul>
+    </li>
+  </ul>
+
+  <div class="legal">
+    <p>&copy; 2024. All rights reserved.</p>
+
+    <div class="legal__links">
+      <span>Made by Nguyen Duy An</span>
+    </div>
+  </div>
+</footer></body>
 </html>
+<!--
+(●っゝω・)っ～☆HELLO☆
+-->
