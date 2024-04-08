ed9b68b295a3595b1c967181cb58825711ed16a9
lea <73567807+lebyleafy@users.noreply.github.com>
2024-04-08 09:44:49 +0700

    Update success.php

--- success.php
+++ success.php
@@ -3,7 +3,7 @@
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" href="table.css?v=1712317897">
+   <link rel="stylesheet" href="styles/style.css?v=1712317897">
 <title>SUCCESS</title>
 </head>
 <body>
