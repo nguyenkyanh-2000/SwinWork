ac0589f79a69e0ecf208cc18b50ed19b579ec7df
lea <73567807+lebyleafy@users.noreply.github.com>
2024-04-08 09:45:47 +0700

    Update search.php

--- search.php
+++ search.php
@@ -5,7 +5,7 @@
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
+   <link rel="stylesheet" href="styles/style.css?v=<?php echo time(); ?>">
 <title>HR Database Manager | SwinWORK</title>
 
 </head>
